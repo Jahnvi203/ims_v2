7d269f730e542225b2dbfae63c82ff625b09f76c
@Jahnvi203 <intern.jahnvi@intelllex.com>
2022-07-15 11:34:21 +0800

    Second commit

--- Home.py
+++ Home.py
@@ -7,6 +7,21 @@ from pandas import ExcelWriter, read_csv
 from copy import deepcopy
 import altair as alt
 from datetime import datetime as dt
+import mysql.connector
+
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+users_rows = run_query("select * from users")
 
 st.title("Hello")
-st.text_input("Name")
+for row in users_rows:
+    st.write(f"{row[0]}'s email is {row[1]}")
